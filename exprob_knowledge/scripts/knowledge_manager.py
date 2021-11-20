599ced970eda233c14db1a470a8f6a7f723f81ec
omotoye <adekoyaomotoye@gmail.com>
2021-11-20 17:16:38 +0000

    :construction: <init> Initialized the robot and camera object package

--- exprob_knowledge/scripts/knowledge_manager.py
+++ exprob_knowledge/scripts/knowledge_manager.py
@@ -12,6 +12,8 @@ from armor_msgs.msg import _ArmorDirectiveReq
 # for generating random hint
 import random
 
+from modules.exception_handling import *  
+
 
 __author__ = "Alessio Capitanelli"
 __copyright__ = "Copyright 2016, ArmorPy"
@@ -21,25 +23,6 @@ __maintainer__ = "Alessio Capitanelli"
 __email__ = "alessio.capitanelli@dibris.unige.it"
 __status__ = "Development"
 
-
-class ArmorPyException(Exception):
-    def __init__(self, message):
-        self.msg = message
-        super(ArmorPyException, self).__init__(self.msg)
-
-
-class ArmorServiceCallError(ArmorPyException):
-    def __init__(self, message):
-        self.msg = message
-        super(ArmorServiceCallError, self).__init__(self.msg)
-
-
-class ArmorServiceInternalError(ArmorPyException):
-    def __init__(self, message, exit_code):
-        self.msg = message
-        self.code = exit_code
-        err_msg = "ARMOR internal error %s: %s" % (self.code, self.msg)
-        super(ArmorServiceInternalError, self).__init__(err_msg)
 
 
 class KnowledgeManager:
---
+++ exprob_knowledge/scripts/modules/exception_handling.py
@@ -0,0 +1,34 @@
+import rospy
+
+# armor custom messages
+from armor_msgs.srv import ArmorDirective, ArmorDirectiveList, ArmorDirectiveListRequest
+from armor_msgs.msg import _ArmorDirectiveReq
+
+
+__author__ = "Alessio Capitanelli"
+__copyright__ = "Copyright 2016, ArmorPy"
+__license__ = "GNU"
+__version__ = "1.0.0"
+__maintainer__ = "Alessio Capitanelli"
+__email__ = "alessio.capitanelli@dibris.unige.it"
+__status__ = "Development"
+
+
+class ArmorPyException(Exception):
+    def __init__(self, message):
+        self.msg = message
+        super(ArmorPyException, self).__init__(self.msg)
+
+
+class ArmorServiceCallError(ArmorPyException):
+    def __init__(self, message):
+        self.msg = message
+        super(ArmorServiceCallError, self).__init__(self.msg)
+
+
+class ArmorServiceInternalError(ArmorPyException):
+    def __init__(self, message, exit_code):
+        self.msg = message
+        self.code = exit_code
+        err_msg = "ARMOR internal error %s: %s" % (self.code, self.msg)
+        super(ArmorServiceInternalError, self).__init__(err_msg)
